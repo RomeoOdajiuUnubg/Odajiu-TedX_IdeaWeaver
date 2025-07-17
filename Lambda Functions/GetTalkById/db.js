e05b03e5b5a0f47d6eb03a503e6812c0b4352bd4
RomeoOdajiuUnubg <r.odajiu@studenti.unibg.it>
2025-07-17 21:24:17 +0200

    Add files via upload

---
+++ Lambda Functions/GetTalkById/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+let isConnected;
+
+// NOTA: In produzione useremo le variabili d'ambiente della Lambda
+const DB_URL = process.env.DB; 
+const DB_NAME = "tedx_weaver_db";
+
+module.exports = connect_to_db = () => {
+    if (isConnected) {
+        console.log('=> using existing database connection');
+        return Promise.resolve();
+    }
+ 
+    console.log('=> using new database connection');
+    return mongoose.connect(DB_URL, {
+        dbName: DB_NAME,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(db => {
+        isConnected = db.connections[0].readyState;
+    });
+};
