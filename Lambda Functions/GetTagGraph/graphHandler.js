b2dbedf613b120ec3040842e6dcaf88e9b4d5bad
RomeoOdajiuUnubg <r.odajiu@studenti.unibg.it>
2025-07-17 21:32:40 +0200

    Update graphHandler.js

--- Lambda Functions/GetTagGraph/graphHandler.js
+++ Lambda Functions/GetTagGraph/graphHandler.js
@@ -1,25 +1,18 @@
-const connect_to_db = require('../shared/db');
-const TagGraph = require('../shared/models/TagGraph');
+const connect_to_db = require('./db');
+const TagGraph = require('./TagGraph');
 
-/**
- * Handler per l'endpoint GET /graph/tags
- * Recupera l'intero set di dati delle connessioni tra i tag.
- */
 module.exports.getTagGraph = async (event) => {
     await connect_to_db();
 
     try {
-        // Esegue una query per trovare tutti i documenti nella collection
         const graphData = await TagGraph.find({});
 
-        // Risposta di successo
         return {
             statusCode: 200,
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(graphData),
         };
     } catch (err) {
-        // Gestione degli errori
         console.error("Errore durante il recupero del grafo dei tag:", err);
         return {
             statusCode: 500,
