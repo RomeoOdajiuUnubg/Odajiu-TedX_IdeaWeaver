b2a41f962a3fe5a4529c32e928fd1f2daf545181
RomeoOdajiuUnubg <r.odajiu@studenti.unibg.it>
2025-07-17 21:42:53 +0200

    Update graphHandler.js

--- Lambda Functions/GetTagGraph/graphHandler.js
+++ Lambda Functions/GetTagGraph/graphHandler.js
@@ -2,21 +2,21 @@ const connect_to_db = require('./db');
 const TagGraph = require('./TagGraph');
 
 module.exports.getTagGraph = async (event) => {
-    await connect_to_db();
+  await connect_to_db();
 
-    try {
-        const graphData = await TagGraph.find({});
+  try {
+    const graphData = await TagGraph.find({});
 
-        return {
-            statusCode: 200,
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(graphData),
-        };
-    } catch (err) {
-        console.error("Errore durante il recupero del grafo dei tag:", err);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: 'Impossibile recuperare il grafo dei tag.', error: err.message }),
-        };
-    }
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(graphData),
+    };
+  } catch (err) {
+    console.error("Errore durante il recupero del grafo dei tag:", err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Impossibile recuperare il grafo dei tag.', error: err.message }),
+    };
+  }
 };
